3fad44fbf1ea5da631e7a6c22084be21b41b8728
EliseevVadim <eliseevv02@mail.ru>
2019-12-22 22:59:38 +0300

    L4V6T3

---
+++ 4.6.3/4.6.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _4._6._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            RegularFinding(str);
+        }
+        static void RegularFinding (string s)
+        {
+            Regex regular = new Regex(@"\w+:\s*[int|short|byte]+\s*\[\d+\]");
+            foreach (Match match in regular.Matches(s))
+            Console.WriteLine(match);
+        }
+    }
+}
